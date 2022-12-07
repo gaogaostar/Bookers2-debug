6169962140843962f4b3ce3fcf2c7d3eb9d928a6
gaogaostar <115141870+gaogaostar@users.noreply.github.com>
2022-12-07 13:09:05 +0000

    [Finish]応用課題6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+    # 非同期通信にするため削除 redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorire = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorire = current_user.favorites.find_by(book_id: @book.id)
     favorire.destroy
-    redirect_back(fallback_location: root_path)
+    # 削除 redirect_back(fallback_location: root_path)
   end
 end
