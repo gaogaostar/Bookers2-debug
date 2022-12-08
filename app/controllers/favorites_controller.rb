205523ec28eafa3838ad6e28df9d0a1c4f3ecf3a
gaogaostar <115141870+gaogaostar@users.noreply.github.com>
2022-12-08 15:37:09 +0000

    [Add]Raty

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,17 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
     # 非同期通信にするため削除 redirect_back(fallback_location: root_path)
+    # redirectを削除したので、app/views/favorites/create.js.erbを探しに行く
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorire = current_user.favorites.find_by(book_id: @book.id)
+    favorire = @book.favorites.find_by(user_id: current_user.id)
     favorire.destroy
     # 削除 redirect_back(fallback_location: root_path)
   end
